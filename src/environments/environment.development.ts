968984368430ef16ac669c3f1a8b38bc0925eca4
yaashall01 <yassinechalh9@gmail.com>
2024-02-07 10:41:05 +0100

    Api update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
-  
+
   featureFlag: true,
   debugMode: true,
-  urlApiC :"http://38.242.131.85:8057/api/categories",
-  urlApiP : "http://38.242.131.85:8057/api/products",
-  urlApiV : "http://38.242.131.85:8057/api/varietys",
-  urlApiS : "http://38.242.131.85:8057/api/suppliers",
-  urlApiO : "http://38.242.131.85:8059/api/v1/orders",
-  urlApiD : "http://38.242.131.85:8059/api/v1/delivery-persons",
-  urlApiT : "http://38.242.131.85:8058/api/v1"
+  urlApiC: 'http://38.242.131.85:8057/catalog-service/v2/api/categories',
+  urlApiP: 'http://38.242.131.85:8057/catalog-service/v2/api/products',
+  urlApiV: 'http://38.242.131.85:8057/catalog-service/v2/api/varietys',
+  urlApiS: 'http://38.242.131.85:8057/catalog-service/v2/api/suppliers',
+  urlApiO: 'http://38.242.131.85:8059/livraison-service/v2/api/v1/orders',
+  urlApiD: 'http://38.242.131.85:8059/livraison-service/v2/api/v1/delivery-persons',
+  urlApiT: 'http://38.242.131.85:8058/stock-service/v2/api/v1',
 };
